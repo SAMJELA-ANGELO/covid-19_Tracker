38d814701a5c585a90a3bc60db360e7201b6b809
Samjela Angelo <samjelaangelo@gmail.com>
2023-03-08 12:49:37 +0100

    Covid Tracker completed

---
+++ src/components/SearchCountry.vue
@@ -0,0 +1,29 @@
+<!-- eslint-disable vue/require-v-for-key -->
+<template>
+    <div>
+    <select @change="onChange" v-model="selected" class="form-select mt-1 block w-full border p-3 rounded">
+    // eslint-disable-next-line vue/require-v-for-key
+    <option v-for="country in countries" :value="country.ID" >
+        {{ country.Country }}</option></select>
+    </div>
+</template>
+
+<script>
+export default{
+    name:'SearchCountry',
+    props: ['countries'],
+    data(){
+        return{
+            selected:0
+        }
+        
+    },
+    methods:{
+        onChange(){
+            const country = this.countries.find((item)=>item.ID ===this.selected)
+            this.$emit('get-country', country)
+
+        }
+    }
+}
+</script>
